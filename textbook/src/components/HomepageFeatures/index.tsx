fce4c06537439f428d6d33421bd35be6805c707c
Patrick Coxall <mr.coxall@mths.ca>
2023-03-14 20:34:58 +0000

    image update

--- textbook/src/components/HomepageFeatures/index.tsx
+++ textbook/src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,20 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Textbook',
+    Svg: require('@site/static/img/undraw_notebook_re_id0r.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Visit the online textbook.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Code Examples',
+    Svg: require('@site/static/img/undraw_pair_programming_re_or4x.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Goto the GitHub repo that has all the code examples.
       </>
     ),
   },
