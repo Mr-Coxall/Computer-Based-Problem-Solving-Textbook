73b37124f6a9671b17940886ca3e8681830615d3
Mr Coxall <mr.coxall@mths.ca>
2023-06-16 18:50:01 +0000

    update

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -4,10 +4,21 @@
 cp ./.devcontainer/.bashrc ~/.bashrc
 sudo apt update
 sudo apt -y full-upgrade
-pip3 install cpplint
+sudo pip3 install cpplint
 
 sudo mkdir /usr/share/fonts/truetype/ComicMono
 sudo cp ./.devcontainer/ComicMono.ttf /usr/share/fonts/truetype/ComicMono
+
+# install Charm VHS
+sudo apt install -y ffmpeg
+sudo apt-get -y install build-essential cmake git libjson-c-dev libwebsockets-dev
+git clone https://github.com/tsl0922/ttyd.git
+cd ttyd && mkdir build && cd build
+cmake ..
+make && sudo make install
+go install github.com/charmbracelet/vhs@latest
+cd ../..
+sudo rm -R ./ttyd
 
 git config --global user.name "Mr Coxall"
 git config --global user.email "mr.coxall@mths.ca"
