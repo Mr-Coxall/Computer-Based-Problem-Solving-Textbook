41050464151fb62169454d4ba38f0d754296eb23
Mr Coxall <mr.coxall@mths.ca>
2023-06-16 18:34:01 +0000

    update

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -2,7 +2,15 @@
 
 # Copy .bashrc to home directory
 cp ./.devcontainer/.bashrc ~/.bashrc
+sudo apt update
+sudo apt -y full-upgrade
+pip3 install cpplint
+
+sudo mkdir /usr/share/fonts/truetype/ComicMono
+sudo cp ./.devcontainer/ComicMono.ttf /usr/share/fonts/truetype/ComicMono
+
 git config --global user.name "Mr Coxall"
 git config --global user.email "mr.coxall@mths.ca"
 # shellcheck source=/dev/null
 source ~/.bashrc 
+git config pull.rebase false
