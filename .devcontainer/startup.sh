b52c5adf37928827a0b62ae3293656709a0a7df6
Mr Coxall <mr.coxall@mths.ca>
2023-06-16 18:31:52 +0000

    update

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -2,5 +2,7 @@
 
 # Copy .bashrc to home directory
 cp ./.devcontainer/.bashrc ~/.bashrc
+git config --global user.name "Mr Coxall"
+git config --global user.email "mr.coxall@mths.ca"
 # shellcheck source=/dev/null
 source ~/.bashrc 
