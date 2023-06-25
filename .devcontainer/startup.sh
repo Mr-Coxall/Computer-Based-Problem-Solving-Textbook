a7dcf347cc6a08c9e5cb42edd6eb821f36a441a3
Patrick Coxall <mr.coxall@mths.ca>
2023-06-24 23:14:33 -0400

    lint fix

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -13,7 +13,9 @@ sudo cp ./.devcontainer/ComicMono.ttf /usr/share/fonts/truetype/ComicMono
 sudo apt install -y ffmpeg
 sudo apt-get -y install build-essential cmake git libjson-c-dev libwebsockets-dev
 git clone https://github.com/tsl0922/ttyd.git
-cd ttyd && mkdir build && cd build
+cd ttyd
+mkdir build
+cd build
 cmake ..
 make && sudo make install
 go install github.com/charmbracelet/vhs@latest
