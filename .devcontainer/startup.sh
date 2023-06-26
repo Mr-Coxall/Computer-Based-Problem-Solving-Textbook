661173175cdc3b1c13cc556c7f1049371cbc5f5f
Patrick Coxall <mr.coxall@mths.ca>
2023-06-26 08:43:32 -0400

    linting fix

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy .bashrc to home directory
-cp ./.devcontainer/.bashrc ~/.bashrc
+cp ./.devcontainer/.bashrc ~/.bashrc || exit
 sudo apt update
 sudo apt -y full-upgrade
 sudo pip3 install cpplint
@@ -13,9 +13,9 @@ sudo cp ./.devcontainer/ComicMono.ttf /usr/share/fonts/truetype/ComicMono
 sudo apt install -y ffmpeg
 sudo apt-get -y install build-essential cmake git libjson-c-dev libwebsockets-dev
 git clone https://github.com/tsl0922/ttyd.git
-cd ttyd
+cd ttyd || exit
 mkdir build
-cd build
+cd build || exit
 cmake ..
 make && sudo make install
 go install github.com/charmbracelet/vhs@latest
