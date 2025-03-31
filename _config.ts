fef428811f7808d4a9e3be395afd76d480941ec3
Mr Coxall <mr.coxall@mths.ca>
2025-03-31 20:56:39 +0000

    update

--- _config.ts
+++ _config.ts
@@ -1,11 +1,11 @@
 import lume from "lume/mod.ts";
 import lumocs from "lumocs/mod.ts";
 
-//const siteUrl = "https://url.of.site";
+const siteUrl = "https://github.com/Mr-Coxall/Computer-Based-Problem-Solving-Textbook";
 
 const site = lume({
   src: "./docs/src",
-  //location: new URL(siteUrl),
+  location: new URL(siteUrl),
 });
 
 site.use(lumocs());
